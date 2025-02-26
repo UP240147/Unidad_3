5387e643b996a3e18cfd6179042613c8d076730d
UP240147 <up240147@alumnos.upa.edu.mx>
2025-02-26 16:58:54 -0600

    Add files via upload

---
+++ 02_Day2exercise.py
@@ -0,0 +1,96 @@
+#Dia dos con Phyton
+
+print('Hello, World')
+print(len('Hello, World'))
+print(type('Hello, World'))
+print(str(10))
+print(int('10'))
+print(float(10))
+input('Enter your name:')
+
+#Variables en phyton
+
+primer_nombre = 'Joel'
+apellidos = 'Marmolejo Lopez'
+pais = 'Mexico'
+ciudad = 'Aguascalientes'
+edad = 19 
+estas_casado = False
+habilidades = {'Phyton', 'Arduino'}
+informacion_personal =  {
+    'first_name':'Joel',
+    'last_name':'Marmolejo Lopez',
+    'country':'Mexico',
+    'city':'Aguascalintes'
+    }
+
+print ('hello' ,',', 'World','!')
+
+print('Mi nombre es:', primer_nombre)
+print('Mis apellidos son:', apellidos)
+print('mi pais es:', pais)
+print('mi ciudad es:', ciudad)
+print('mi edad:', edad)
+print('¿estoy casado?', estas_casado)
+print('mis habilidades', habilidades)
+print('mi informacion personal', informacion_personal)
+
+#Getting user input using the input() built-in funtion.
+
+first_name = input('¿Cual es tu nombre?')
+age = input('¿Cuantos años tienes?')
+
+print(first_name)
+print(age)
+
+import math
+#Ejercisio nivel 1
+
+print('Dia 2: 30 Days of python programming')
+first_Name = "Joel"
+last_Name = "Marmolejo Lopez"
+full_Name = "Joel Marmolejo Lopez"
+country = "Mexico"
+city = "Aguascalientes"
+age = 19
+year = 2025
+is_Married = False
+is_True = True
+is_Light_On = False
+skills, favorite_Book, favorte_Movie, dad_Age = "Python", "Metamorfosis", "Resident evil 3", 44
+
+#Ejercisio nivel 2
+
+print(type('Dia 2: 30 Days of python programming'))
+print(type("Joel"))
+print(type("Marmolejo lopez"))
+print(type("Joel Marmolejo Lopez"))
+print(type("Mexico"))
+print(type("Aguascalientes"))
+print(type(19))
+print(type(2025))
+print(type(False))
+print(type(True))
+print(type(False))
+
+print(len("Joel"))
+print(len("Marmolejo lopez"))
+num1, num2 = 5, 4
+total = num1 + num2
+diff = num2 - num1
+product = num1 * num2
+division = num1 / num2
+remainder = num1 % num2
+exp = num1 ** num2
+floor_division = num1 // num2
+area_of_circle = math.pi * 30**2
+circum_of_circle = 2 * math.pi * 30
+radius = float(input("Put the radius "))
+area = math.pi * radius**2
+print("Your area is ", area)
+first_Namee = input("Put your name")
+name_last = input("Put your last name")
+countrys = input("Put your country")
+ages = input("Put your age")
+
+help("keywords")
