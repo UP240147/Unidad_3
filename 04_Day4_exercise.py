22b52f963fb91cc8868176684c00ddbe8cee87b3
UP240147 <up240147@alumnos.upa.edu.mx>
2025-03-10 14:15:59 -0600

    Add files via upload

---
+++ 04_Day4_exercise.py
@@ -0,0 +1,181 @@
+# Ejercicio 1
+Word1 , Word2 , Word3 , Word4 = 'Thirty','Days','Of', 'Phyton'
+single_string = Word1 + " " + Word2 + " " + Word3 + " " + Word4
+print(single_string)
+
+# Ejercicio 2
+Word_1 , Word_2 , Word_3 = 'Coding','For','All'
+single_string2 = Word_1 + " " + Word_2 + " " + Word_3 + "."
+print(single_string2)
+
+# Ejercicio 3
+company = 'Coding For All'
+
+# Ejercicio 4
+print(company)
+
+# Ejercicio 5
+print(len(company))
+
+# Ejercicio 6
+text = 'company'
+A_word = text.upper()
+print(A_word)
+
+# Ejercicio 7
+text_2 = 'SCHOOL'
+B_word = text_2.lower()
+print(B_word)
+
+# Ejercicio 8
+text_3 = 'afuera de mi casa hay un puesto de tacos'
+print(text_3.capitalize())
+print(text_3.title())
+print(text_3.swapcase())
+
+# Ejercicio 9
+text_4 = 'Coding For All'
+palabra = text_4.replace('Coding', ' ')
+print(palabra)
+
+# Ejercico 10
+if text_4.find('Coding') == 0:
+    print(True)
+else:
+    print(False)
+
+#EJERCICIO 11
+replaceWord = text_4.replace("Coding" , "Python")
+print(replaceWord)
+
+#EJERCICIO 12
+moreReplaceWord = replaceWord.replace("Python", "Everyone").replace("All", "Python")
+print(moreReplaceWord)
+
+#EJERCICIO 13
+splitText = text_4.split()
+print(splitText)
+#Esto hace que genere una lista espaciada
+#Existen diferentes párametros que pueden ponerse en el .split()
+#Algunos parametros son split.(separador, maxsplit , "," , " ", )
+
+#EJERCICIO 14
+empresas = "Facebook, Google, Microsoft, Apple, IBM, ORacle, Amazon"
+splitEmpresas = empresas.split(",")
+print(splitEmpresas)
+
+#EJERCICIO 15
+forAll = "Coding For All"
+onlyOne = forAll[0]
+print(onlyOne)
+
+#EJERCICIO 16
+lastOne = forAll[-1] #Al ser un index negativom empieza desde atras
+print(lastOne)
+
+#EJERCICIO 17
+idexten = forAll[10]
+print(idexten)
+
+#EJERCICIo 18
+textoo = "Python For Everyone"
+acronym = "".join(word[0] for word in textoo.split())
+print(acronym)
+
+#EJERCICIO 19
+mastexto = "Coding For All"  #Declaras la variable de tu texto
+masacronimos = "".join(word[0] for word in mastexto.split()) 
+print(masacronimos)
+
+#EJERCICIO 20
+eltexto = "Coding For All"
+position = eltexto.index("C")
+print(position)
+
+#EJERCICIO 21
+position2 = eltexto.index("F")
+print(position2)
+
+#EJERCICIO 22
+eltesto = "Coding For All"
+encontrareltesto = eltesto.find("l")
+print(encontrareltesto)
+
+#EJERCICIO 23
+text = "You cannot end a sentence with because because because is a conjunction"
+posicion = text.find("because") 
+print(posicion)
+
+#EJERCICIO 24
+text = "You cannot end a sentence with because because because is a conjunction"
+anotherposition = text.rindex("because")-1
+print(anotherposition)
+
+#EJERCICIO 25
+textocortado = "You cannot end a sentence with because because because is a conjunction"
+#Lo reemplazara por un espacio vacío
+textoepico = textocortado.replace("because because because", "")
+print(textoepico)
+
+#EJERCICIO 26
+text = "You cannot end a sentence with because because because is a conjunction"
+posicion = text.find("because") 
+print(posicion)
+
+#EJERCICIO 27
+textocortado = "You cannot end a sentence with because because because is a conjunction"
+textoepico = textocortado.replace("because because because", "")
+print(textoepico)
+ 
+#EJERCICIO 28
+substring = "Coding For All"
+if substring.find("Coding") == 0:
+    print(True)
+else: print(False)
+
+#EJERCICIO 29
+substring = "Coding For All"
+if substring.find("coding") == 11:
+    print(True)
+else: print(False)
+
+#EJERCICIO 30
+removeSpaces = "   Coding For All   "
+removidosJeje = removeSpaces.replace("   ", "")
+print(removidosJeje)
+
+#EJERCICIO 31
+var1 = '30DaysOfPython'
+print(var1.isidentifier()) # Falso
+var2 = 'thirty_days_of_python'
+print(var2.isidentifier()) # Verdadero
+# La función isidentifier checa si puede o no ser un nombre valido para crear una variable
+
+#EJERCICIO 32
+librerias = ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']
+# Unir la lista con "# " como separador
+result = " # ".join(librerias)
+print(result)
+
+#EJERCICIO 33
+separar = "I am enjoying this challenge.\nI just wonder what is next."
+print(separar)
+#La función \n hace un espacio en diferentes lineas
+
+#EJERCICIO 34
+tabular = "Name\tAge\tCountry\tCity\nAsabeneh\t250\tFinland\tHelsinki"
+print(tabular)
+#La función \t sirve para que tabule las palabras y esten alineadas correctamente
+
+#EJERCICIO 35
+radius = 10
+area = 3.14 * radius ** 2
+# Formateando con f-string
+formatear = f"The area of a circle with radius {radius} is {area} meters square."
+print(formatear)
+#La letra f hace que toda la cadena siguiente se haga un string
+#{} todo dentro de la llave sera visto como una expresión
+
+#EJERCICIO 36
+masformateointenso = f"Las operaciones aritmeticas de 8 y 6 son su suma de {8+6},\n su resta es {8-6}, la multiplicación de \n{8*6}, su division de {8%6}, su {8//6} y su potencia de {8**6} "
+print(masformateointenso)
