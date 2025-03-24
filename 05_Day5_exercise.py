a3f0ec79f993054c503bb7ff6a52a6c021eb6b1e
UP240147 <up240147@alumnos.upa.edu.mx>
2025-03-24 14:49:16 -0600

    05_Day5_exercise.py

--- 05_Day5_exercise.py
+++ 05_Day5_exercise.py
@@ -1,109 +1,109 @@
 
-#EJERCICIO 1
+# EJERCICIO 1
 empytList = []
 
-#EJERCICIO 2
+# EJERCICIO 2
 list5_items = ["Hola" , 4 , True, "Adios", 12j]
 
-#EJERCICIO 3
+# EJERCICIO 3
 print(len(list5_items))
 
-#EJERCICIO 4
+# EJERCICIO 4
 getTheItem = [list5_items[i] for i in [0, 2, 4]]
 print(getTheItem)  
 #Selecciona el lugar y los imprime
 
-#EJERCICIO 5
+# EJERCICIO 5
 mixed_data_types = ["José Ángel", 18, 1.78, "Single", "Que te importa"]
 
-#EJERCICIO 6
+# EJERCICIO 6
 it_companies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"]
 
-#EJERCICIO 7
+# EJERCICIO 7
 print(it_companies)
 
-#EJERCICIO 8
+# EJERCICIO 8
 print(len(it_companies))
 
-#EJERCICIO 9 
+# EJERCICIO 9 
 getTheCompanies = [it_companies[i] for i in [0,3, 6]]
 
-#EJERCICIO 10
+# EJERCICIO 10
 it_companies[0] = "Removido"
 print(it_companies)
 
-#EJERCICIO 11
+# EJERCICIO 11
 it_companies.insert(1, "Agregado")
 print(it_companies)
 
-#EJERCICIO 12
+# EJERCICIO 12
 it_companies.insert(4, "IT ")
 print(it_companies)
 
-#EJERCICIO 13
+# EJERCICIO 13
 it_companies = [company.upper() for company in it_companies]
 print(it_companies)
 
-#EJERCICIO 14
+# EJERCICIO 14
 listaUnida = "#;  ".join(it_companies)
 print(listaUnida)
 
-#EJERCICIO 15
+# EJERCICIO 15
 it_companies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"]
 if it_companies[1] == "Google":
     print("Existe")
 else: print("No Existe")
 
-#EJERCICIO 16
+# EJERCICIO 16
 it_companies.sort()
 print(it_companies)
 
-#EJERCICIO 17
+# EJERCICIO 17
 it_companies.reverse()
 print(it_companies)
 
-#EJERCICIO 18
+# EJERCICIO 18
 it_companies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"]
 sliceFirstCompanies = it_companies[:3]
 print(sliceFirstCompanies)
 
-#EJERCICIO 19
+# EJERCICIO 19
 sliceLastCompanies = it_companies[3:]
 print(sliceLastCompanies)
 
-#EJERCICIO 20
+# EJERCICIO 20
 it_companies = ["Facebook", "Google", "Microsoft", "IT", "Apple", "IBM", "Oracle", "Amazon"]
 it_companies.remove("IT")
 print(it_companies)
 
-#EJERCICIO 21
+# EJERCICIO 21
 it_companies.remove("Facebook")
 print(it_companies)
 
-#EJERCICIO 22
+# EJERCICIO 22
 it_companies.remove("Apple")
 print(it_companies)
 
-#EJERCICIO 23
+# EJERCICIO 23
 it_companies.remove("Amazon")
 print(it_companies)
 
-#EJERCICIO 24
+# EJERCICIO 24
 it_companies.clear()
 print(it_companies)
 
-#EJERCICIO 25
+# EJERCICIO 25
 it_companies = None
 print(it_companies)
 
-#EJERCICIO 26
+# EJERCICIO 26
 front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
 back_end = ['Node', 'Express', 'MongoDB']
 # Unir las listas
 full_stack = front_end + back_end
 print(full_stack)
 
-#EJERCICIO 27
+# EJERCICIO 27
 full_stack_copy = full_stack.copy()  # Copiar la lista unida
 # Encotrar el índice de "Redux" y luego insertar los nuevos elementos después de él
 redux_index = full_stack_copy.index("Redux")
@@ -112,7 +112,7 @@ full_stack_copy.insert(redux_index + 1, "Python")
 full_stack_copy.insert(redux_index + 2, "SQL")
 print(full_stack_copy)
 
-#LEVEL 2 EXERCISES
+# Nivel 2 de ejercicios
 ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
 ages.sort()
 print(ages)
@@ -343,11 +343,11 @@ if num_countries % 2 == 0:
 else:
     middle_countries = [countries[middle_index]]
 
-# Dividir la lista en dos mitades
+# Ejercicio 29 Dividir la lista en dos mitades
 first_half = countries[:middle_index + (1 if num_countries % 2 != 0 else 0)]
 second_half = countries[middle_index + (1 if num_countries % 2 != 0 else 0):]
 
-# Desempaquetar los tres primeros países y asignar el resto como países Escandinavos
+# Ejercicio 30 Desempaquetar los tres primeros países y asignar el resto como países Escandinavos
 first_three, *scandic_countries = first_half[:3], *second_half
 
 print("Middle country(ies):", middle_countries)
